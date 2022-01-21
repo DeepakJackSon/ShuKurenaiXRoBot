917027c5481df5ac7db7a2d775b7d6f1986a9916
SHU KURENAI TEAM <94837807+shukurenai007@users.noreply.github.com>
2022-01-21 17:20:06 +0530

    Add files via upload

--- ShuKurenaiXRoBot/modules/sql/__init__.py
+++ ShuKurenaiXRoBot/modules/sql/__init__.py
@@ -1,1 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
 
+from ShuKurenaiXRoBot import DB_URL
+
+
+def start() -> scoped_session:
+    engine = create_engine(DB_URL, client_encoding="utf8")
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=True))
+
+
+BASE = declarative_base()
+SESSION = start()
