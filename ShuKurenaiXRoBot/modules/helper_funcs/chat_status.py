14c202abf211afd3aa037131228087635a9eb6f2
SHU KURENAI TEAM <94837807+shukurenai007@users.noreply.github.com>
2022-01-22 11:56:00 +0530

    Update chat_status.py

--- ShuKurenaiXRoBot/modules/helper_funcs/chat_status.py
+++ ShuKurenaiXRoBot/modules/helper_funcs/chat_status.py
@@ -403,6 +403,6 @@ def connection_status(func):
 
 
 # Workaround for circular import with connection.py
-from EmikoRobot.modules import connection
+from ShuKurenaiXRoBot.modules import connection
 
 connected = connection.connected
