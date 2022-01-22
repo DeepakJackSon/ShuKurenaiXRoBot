95a4e247112934433c4619794713f12778254539
SHU KURENAI TEAM <94837807+shukurenai007@users.noreply.github.com>
2022-01-22 12:08:13 +0530

    Update forceSubscribe_sql.py

--- ShuKurenaiXRoBot/modules/sql/forceSubscribe_sql.py
+++ ShuKurenaiXRoBot/modules/sql/forceSubscribe_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, String, Numeric, Boolean
-from EmikoRobot.modules.sql import BASE, SESSION
+from ShuKurenaiXRoBot.modules.sql import BASE, SESSION
 
 
 class forceSubscribe(BASE):
