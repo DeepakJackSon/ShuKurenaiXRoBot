f89efcb08cb362c0ca5307af84c96a1d8278a64f
SHU KURENAI TEAM <94837807+shukurenai007@users.noreply.github.com>
2022-01-23 18:55:55 +0530

    Update nightmode.py

--- ShuKurenaiXRoBot/modules/nightmode.py
+++ ShuKurenaiXRoBot/modules/nightmode.py
@@ -167,3 +167,12 @@ async def job_open():
 scheduler = AsyncIOScheduler(timezone="Asia/Jakarta")
 scheduler.add_job(job_open, trigger="cron", hour=5, minute=58)
 scheduler.start()
+
+__help__ = f"""
+ 🔹 `/nightmode` on/off
+ 
+**Note:** Night Mode chats get Automatically closed at 12pm(IST)
+and Automatically openned at 6am(IST) To Prevent Night Spams.
+"""
+
+__mod_name__ = "Night Mode"
