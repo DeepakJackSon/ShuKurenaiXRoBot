f743924ff55afd688b4655fdc897e42ca0ef66e1
SHU KURENAI TEAM <94837807+shukurenai007@users.noreply.github.com>
2022-01-26 19:56:48 +0530

    Update users.py

--- ShuKurenaiXRoBot/modules/users.py
+++ ShuKurenaiXRoBot/modules/users.py
@@ -146,7 +146,7 @@ def chats(update: Update, context: CallbackContext):
 def chat_checker(update: Update, context: CallbackContext):
     bot = context.bot
     try:
-        if update.effective_message.chat.get_member(bot.id).can_send_messages is False:
+        if update.effective_message.chat.get_chat_member(bot.id).can_send_messages is False:
             bot.leaveChat(update.effective_message.chat.id)
     except Unauthorized:
         pass
