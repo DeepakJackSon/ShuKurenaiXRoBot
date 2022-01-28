781199aaccab33f21712ea7014ef37bf76d008d1
SHU KURENAI TEAM <94837807+shukurenai007@users.noreply.github.com>
2022-01-28 17:59:37 +0530

    Update config.py

--- ShuKurenaiXRoBot/config.py
+++ ShuKurenaiXRoBot/config.py
@@ -36,6 +36,8 @@ class Config(object):
     URL = None
     SPAMWATCH_API = ""  # go to support.spamwat.ch to get key
     SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
+    SESSION_STRING = None
+    STRING_SESSION = None
 
     # OPTIONAL
     ##List of id's -  (not usernames) for users which have sudo access to the bot.
